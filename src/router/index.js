279443428849a79a60b7817aabeb501b84a25e9e
TiaAdam <44812614+TiaAdam@users.noreply.github.com>
2020-11-09 13:24:53 +0000

    Add files via upload

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import CartPage from '../views/CartPage.vue'
+import ProductDetailPage from '../views/ProductDetailPage.vue'
+import ProductsPage from '../views/ProductsPage.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/products',
+    name: 'Products',
+    component: ProductsPage,
+  }, {
+    path: '/products/:id',
+    name: 'ProductDetail',
+    component: ProductDetailPage,
+  }, {
+      path: '/cart',
+      name: 'Cart',
+      component: CartPage,
+  }, {
+      path: '/',
+      redirect: '/products', 
+      // this  code will redirect user to localhost:8081 and will not display any errors
+
+  }
+] 
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
